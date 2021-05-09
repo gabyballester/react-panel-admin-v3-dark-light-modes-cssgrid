4f7494f118cd4e52c3ee9a411ef9b13aa9dab8ea
Gabriel Ballester <gaby.ballester@gmail.com>
2021-05-09 18:44:06 +0200

    Create nav, side, main components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Fragment } from 'react';
+import Main from './components/Main';
+
+import './App.css';
+
+import NavBar from "./components/NavBar";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <h1>Panel</h1>
-    );
+    <Fragment>
+      <NavBar />
+      <Sidebar />
+      <Main />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+
+    // nav left
+    <div className="navbar">
+      <ul className="navbar-nav nav-left">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <i className="fas fa-bars"></i>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <i className="fas fa-bars"></i>
+          </a>
+        </li>
+      </ul>
+      {/* end nav left */}
+
+      {/* form */}
+      <form className="navbar-search">
+        <input type="text" name="Search" className="navbar-search-input" placeholder="What you looking for..." />
+        <i class="fas fa-search"></i>
+      </form>
+      {/* end form */}
+
+      {/* nav left */}
+      <ul className="navbar-nav nav-right">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <i className="fas fa-bars"></i>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <i className="fas fa-bars"></i>
+          </a>
+        </li>
+      </ul>
+      {/* end nav left */}
+
+    </div>
+  )
+}
+
+export default NavBar;
