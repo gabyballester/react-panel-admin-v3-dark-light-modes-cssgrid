d84ae2c7346b9f8702c112449568822e1e768d90
Gabriel Ballester <gaby.ballester@gmail.com>
2021-05-09 22:17:34 +0200

    Sidebar styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import './Main.css';
 
 const Main = () => {
   return (
-    <div>
+    <div className="wrapper">
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
       
+      <p><strong>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium beatae eum nulla iure eius rerum sequi nemo aliquid voluptas cupiditate, corporis, a omnis fuga labore assumenda eos distinctio tenetur asperiores.
+        </strong></p>
+
+      <h2>Header level 2</h2>
+
+      <ol>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+        <li>Debitis hic non libero dolores velit eius dolor asperiores </li>
+      </ol>
+
+      <blockquote>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A harum placeat facilis delectus alias qui sunt minus quam dolorum doloribus necessitatibus molestiae voluptate fugit officiis nihil, impedit perferendis eveniet reprehenderit?</p>
+      </blockquote>
+
+      <h3>Header level 3</h3>
+
+      <ul>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+        <li>Debitis hic non libero dolores velit eius dolor asperiores </li>
+      </ul>
+
+      <pre>
+        header h1 a 
+        display block;
+        width: 300px;
+        height: 80px;
+      </pre>
+
     </div>
   )
 }
