5184a5c4f98e57366d44f215973495f5abaecb47
Gabriel Ballester <gaby.ballester@gmail.com>
2021-05-10 15:39:58 +0200

    Counter row

---
+++ src/components/MainOld.js
@@ -0,0 +1,44 @@
+import './Main.css';
+
+const Main = ({ showSidebar }) => {
+
+  const sidebarContainer = `wrapper ${showSidebar ? 'expand' : ''}`;
+
+  return (
+    <div className={sidebarContainer}>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+
+      <p><strong>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium beatae eum nulla iure eius rerum sequi nemo aliquid voluptas cupiditate, corporis, a omnis fuga labore assumenda eos distinctio tenetur asperiores.
+        </strong></p>
+
+      <h2>Header level 2</h2>
+
+      <ol>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+        <li>Debitis hic non libero dolores velit eius dolor asperiores </li>
+      </ol>
+
+      <blockquote>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A harum placeat facilis delectus alias qui sunt minus quam dolorum doloribus necessitatibus molestiae voluptate fugit officiis nihil, impedit perferendis eveniet reprehenderit?</p>
+      </blockquote>
+
+      <h3>Header level 3</h3>
+
+      <ul>
+        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+        <li>Debitis hic non libero dolores velit eius dolor asperiores </li>
+      </ul>
+
+      <pre>
+        header h1 a
+        display block;
+        width: 300px;
+        height: 80px;
+      </pre>
+
+    </div>
+  )
+}
+
+export default Main
